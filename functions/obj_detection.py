13f64a6a5124998c8a78d7cdc2a6daa4ba560f43
simeon9696 <simeon.ramjit@my.uwi.edu>
2020-09-14 18:11:59 -0400

    added obj_detection.py

---
+++ functions/obj_detection.py
@@ -0,0 +1,11 @@
+from detecto import core, utils, visualize
+import matplotlib.pyplot as plt
+import os
+
+# Directory at which the model is stored
+MODEL_PATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'model','model_weights.pth'))
+CLASS_NAMES_PATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'model','class_names.txt'))
+CLASS_NAMES =  open(CLASS_NAMES_PATH, "r").readline().split(',')
+
+# Use the saved model to detect objects in images
+# model = core.Model.load(MODEL_PATH, CLASS_NAMES)
