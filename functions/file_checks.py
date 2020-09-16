35ba27670e032e06bfe4b81854206fdde7ef1db9
simeon9696 <simeon.ramjit@my.uwi.edu>
2020-09-16 13:09:48 -0400

    moved helper functions to separate file

---
+++ functions/file_checks.py
@@ -0,0 +1,111 @@
+from detecto import core, utils, visualize
+import matplotlib.pyplot as plt
+import os
+from datetime import datetime
+import time
+
+import shutil
+
+
+def check_dirs(IMAGE_PATH, LABEL_PATH):
+    # check to see if paths passed in exist or not
+    PATHS_GOOD = False
+
+    try:
+        
+        if not os.path.exists(IMAGE_PATH):
+            MESSAGE = f"{IMAGE_PATH} not found"
+            raise Exception (MESSAGE)
+        elif not os.path.exists(LABEL_PATH):
+            MESSAGE = f"{LABEL_PATH} not found"
+            raise Exception (MESSAGE)
+        else:
+            PATHS_GOOD = True
+            print(f"\x1b[6;37;42m[SUCCESS] Images and labels found !\x1b[0m")
+    except Exception as error:
+        PATHS_GOOD = False
+        print(f"\x1b[6;37;41m[ERROR] {error}\x1b[0m")
+    
+    
+    return PATHS_GOOD
+
+def file_count(IMAGE_PATH,LABEL_PATH):
+
+    IMAGES_COUNT =  len(next(os.walk(IMAGE_PATH))[2])
+    LABELS_COUNT =  len(next(os.walk(LABEL_PATH))[2])
+    COUNT_EQUAL = False
+    try:
+        if not(IMAGES_COUNT == LABELS_COUNT):
+            MESSAGE = f"File count mismatch. {IMAGES_COUNT} images and {LABELS_COUNT} labels. These counts should match"
+            
+            raise Exception(MESSAGE)
+        else:
+            print(f"\x1b[6;37;42m[SUCCESS] Equal number of image and label files !\x1b[0m")
+            COUNT_EQUAL = True
+    except Exception as error:
+        print(f"\x1b[6;37;41m[ERROR] {error}\x1b[0m")
+
+
+    return  COUNT_EQUAL
+
+
+def identify_non_paired_file(IMAGE_PATH, LABEL_PATH):
+    IMAGE_NAMES =   next(os.walk(IMAGE_PATH))[2]
+    LABEL_NAMES =   next(os.walk(LABEL_PATH))[2]
+    IMAGE_NAMES_EXTENSION_STRIPPED = [os.path.splitext(x)[0] for x in IMAGE_NAMES]
+    LABEL_NAMES_EXTENSION_STRIPPED = [os.path.splitext(x)[0] for x in LABEL_NAMES]
+    ALL_FILES_PAIRED = False
+
+    # Find names that only appear in either IMAGE_NAMES only or LABEL_NAMES only
+    NON_PARIED_FILES = list(set(IMAGE_NAMES_EXTENSION_STRIPPED) 
+                            - set(LABEL_NAMES_EXTENSION_STRIPPED ))
+
+    if not NON_PARIED_FILES:
+        ALL_FILES_PAIRED = True
+        print(f"\x1b[6;37;42m[SUCCESS] All image files have a corresponding label file !\x1b[0m")
+    else:
+        print("[INFO] These file names don't exist as a pair i.e they do not have a corresponding label file for the image file or vice versa")
+        print(NON_PARIED_FILES)
+
+    return ALL_FILES_PAIRED
+
+def save_model(MODEL_PATH, MODEL):
+    print(f"\x1b[6;37;42m[SUCCESS] Model trained successfully!\x1b[0m")
+    if not os.path.exists(MODEL_PATH):
+        # If it doesn't exist make it
+        os.mkdir(MODEL_PATH)
+
+        print(f"[INFO] Creating directory to save model in {MODEL_PATH}")
+    else:
+        # Otherwise delete the folder and all of it's contents
+        print(f"[INFO] Removing {MODEL_PATH}")
+        shutil.rmtree(MODEL_PATH)
+        # Then re-create an empty folder
+        
+        print(f"[INFO] Creating directory to save model in {MODEL_PATH}")
+        os.mkdir(MODEL_PATH)
+
+    
+    print(f"[INFO] Saving model in {MODEL_PATH}")
+    MODEL.save(os.path.join(MODEL_PATH, 'model_weights.pth'))
+    return
+    
+def save_class_names(MODEL_PATH,model):
+    CLASS_NAME_PATH = os.path.join(MODEL_PATH, 'class_names.txt')
+    print(f"[INFO] Writing class names to {CLASS_NAME_PATH}")
+    
+    class_name_file = open(CLASS_NAME_PATH,"w") 
+    class_name_file.write(', '.join(CLASS_NAMES))
+    return
+
+def save_losses(MODEL_PATH, losses):
+    LOSSES_PATH = os.path.join(MODEL_PATH, 'losses.txt')
+    print(f"[INFO] Writing loss data to {LOSSES_PATH}")
+    
+    loss_file = open(CLASS_NAME_PATH,"w") 
+    loss_file.write(losses)
+    return
+
+def zip_model_and_loss_and_class_names(MODEL_PATH):
+    shutil.make_archive('model_data', 'zip', MODEL_PATH)
+    return
--- functions/train_obj_model.py
+++ functions/train_obj_model.py
@@ -6,108 +6,8 @@ import time
 
 import shutil
 
-def check_dirs(IMAGE_PATH, LABEL_PATH):
-    # check to see if paths passed in exist or not
-    PATHS_GOOD = False
+import file_checks as fc
 
-    try:
-        
-        if not os.path.exists(IMAGE_PATH):
-            MESSAGE = f"{IMAGE_PATH} not found"
-            raise Exception (MESSAGE)
-        elif not os.path.exists(LABEL_PATH):
-            MESSAGE = f"{LABEL_PATH} not found"
-            raise Exception (MESSAGE)
-        else:
-            PATHS_GOOD = True
-            print(f"\x1b[6;37;42m[SUCCESS] Images and labels found !\x1b[0m")
-    except Exception as error:
-        PATHS_GOOD = False
-        print(f"\x1b[6;37;41m[ERROR] {error}\x1b[0m")
-    
-    
-    return PATHS_GOOD
-
-def file_count(IMAGE_PATH,LABEL_PATH):
-
-    IMAGES_COUNT =  len(next(os.walk(IMAGE_PATH))[2])
-    LABELS_COUNT =  len(next(os.walk(LABEL_PATH))[2])
-    COUNT_EQUAL = False
-    try:
-        if not(IMAGES_COUNT == LABELS_COUNT):
-            MESSAGE = f"File count mismatch. {IMAGES_COUNT} images and {LABELS_COUNT} labels. These counts should match"
-            
-            raise Exception(MESSAGE)
-        else:
-            print(f"\x1b[6;37;42m[SUCCESS] Equal number of image and label files !\x1b[0m")
-            COUNT_EQUAL = True
-    except Exception as error:
-        print(f"\x1b[6;37;41m[ERROR] {error}\x1b[0m")
-
-
-    return  COUNT_EQUAL
-
-
-def identify_non_paired_file(IMAGE_PATH, LABEL_PATH):
-    IMAGE_NAMES =   next(os.walk(IMAGE_PATH))[2]
-    LABEL_NAMES =   next(os.walk(LABEL_PATH))[2]
-    IMAGE_NAMES_EXTENSION_STRIPPED = [os.path.splitext(x)[0] for x in IMAGE_NAMES]
-    LABEL_NAMES_EXTENSION_STRIPPED = [os.path.splitext(x)[0] for x in LABEL_NAMES]
-    ALL_FILES_PAIRED = False
-
-    # Find names that only appear in either IMAGE_NAMES only or LABEL_NAMES only
-    NON_PARIED_FILES = list(set(IMAGE_NAMES_EXTENSION_STRIPPED) 
-                            - set(LABEL_NAMES_EXTENSION_STRIPPED ))
-
-    if not NON_PARIED_FILES:
-        ALL_FILES_PAIRED = True
-        print(f"\x1b[6;37;42m[SUCCESS] All image files have a corresponding label file !\x1b[0m")
-    else:
-        print("[INFO] These file names don't exist as a pair i.e they do not have a corresponding label file for the image file or vice versa")
-        print(NON_PARIED_FILES)
-
-    return ALL_FILES_PAIRED
-
-def save_model(MODEL_PATH, MODEL):
-    print(f"\x1b[6;37;42m[SUCCESS] Model trained successfully!\x1b[0m")
-    if not os.path.exists(MODEL_PATH):
-        # If it doesn't exist make it
-        os.mkdir(MODEL_PATH)
-
-        print(f"[INFO] Creating directory to save model in {MODEL_PATH}")
-    else:
-        # Otherwise delete the folder and all of it's contents
-        print(f"[INFO] Removing {MODEL_PATH}")
-        shutil.rmtree(MODEL_PATH)
-        # Then re-create an empty folder
-        
-        print(f"[INFO] Creating directory to save model in {MODEL_PATH}")
-        os.mkdir(MODEL_PATH)
-
-    
-    print(f"[INFO] Saving model in {MODEL_PATH}")
-    MODEL.save(os.path.join(MODEL_PATH, 'model_weights.pth'))
-    return
-    
-def save_class_names(MODEL_PATH,model):
-    CLASS_NAME_PATH = os.path.join(MODEL_PATH, 'class_names.txt')
-    print(f"[INFO] Writing class names to {CLASS_NAME_PATH}")
-    
-    class_name_file = open(CLASS_NAME_PATH,"w") 
-    class_name_file.write(', '.join(CLASS_NAMES))
-    return
-
-def save_losses(MODEL_PATH, losses):
-    LOSSES_PATH = os.path.join(MODEL_PATH, 'losses.txt')
-    print(f"[INFO] Writing loss data to {LOSSES_PATH}")
-    
-    loss_file = open(CLASS_NAME_PATH,"w") 
-    loss_file.write(losses)
-    return
-
-def zip_model_and_loss_and_class_names(MODEL_PATH):
-    shutil.make_archive('model_data', 'zip', MODEL_PATH)
-    return
 
 '''
 # This shows an image from your training set. 
@@ -122,21 +22,24 @@ LABEL_PATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'tr
 VAL_IMAGE_PATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'validation', 'images'))
 VAL_LABEL_PATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'validation', 'labels'))
 MODEL_PATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'model'))
-PATHS_GOOD = COUNT_EQUAL = ALL_FILES_PAIRED = VAL_PATHS_GOOD = VAL_COUNT_EQUAL = VAL_ALL_FILES_PAIRED = False
+PATHS_GOOD = COUNT_EQUAL = ALL_FILES_PAIRED = VAL_PATHS_GOOD = VAL_COUNT_EQUAL = VAL_ALL_FILES_PAIRED = ALL_CLEAR False
 CLASS_NAMES = ['bat','stumps']
 
 
 print(f"[INFO] Checking testing dataset")
-PATHS_GOOD = check_dirs(IMAGE_PATH,LABEL_PATH)
-COUNT_EQUAL = file_count(IMAGE_PATH,LABEL_PATH)
-ALL_FILES_PAIRED = identify_non_paired_file(IMAGE_PATH,LABEL_PATH)
+PATHS_GOOD = fc.check_dirs(IMAGE_PATH,LABEL_PATH)
+COUNT_EQUAL = fc.file_count(IMAGE_PATH,LABEL_PATH)
+ALL_FILES_PAIRED = fc.identify_non_paired_file(IMAGE_PATH,LABEL_PATH)
 
 print(f"[INFO] Checking validation dataset")
-VAL_PATHS_GOOD = check_dirs(VAL_IMAGE_PATH, VAL_LABEL_PATH)
-VAL_COUNT_EQUAL = file_count(VAL_IMAGE_PATH, VAL_LABEL_PATH)
-VAL_ALL_FILES_PAIRED = identify_non_paired_file(VAL_IMAGE_PATH, VAL_LABEL_PATH)
+VAL_PATHS_GOOD = fc.check_dirs(VAL_IMAGE_PATH, VAL_LABEL_PATH)
+VAL_COUNT_EQUAL = fc.file_count(VAL_IMAGE_PATH, VAL_LABEL_PATH)
+VAL_ALL_FILES_PAIRED = fc.identify_non_paired_file(VAL_IMAGE_PATH, VAL_LABEL_PATH)
 
-if PATHS_GOOD and COUNT_EQUAL and ALL_FILES_PAIRED and VAL_PATHS_GOOD and VAL_COUNT_EQUAL and VAL_ALL_FILES_PAIRED:
+ALL_CLEAR = PATHS_GOOD and COUNT_EQUAL and ALL_FILES_PAIRED and VAL_PATHS_GOOD and VAL_COUNT_EQUAL and VAL_ALL_FILES_PAIRED
+
+'''
+if ALL_CLEAR:
     
     
     print(f"[INFO] All file checks passed, beginning dataset loading !")
@@ -165,3 +68,4 @@ if PATHS_GOOD and COUNT_EQUAL and ALL_FILES_PAIRED and VAL_PATHS_GOOD and VAL_CO
     save_class_names(MODEL_PATH, model)
     save_losses(MODEL_PATH, losses)
     zip_model_and_loss_and_class_names(MODEL_PATH)
+'''
